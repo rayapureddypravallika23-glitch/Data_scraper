4624964e598a57dae8ffcc3c3d97132d8ea86eb0
rayapureddypravallika23-glitch <rayapureddypravallika23@gmail.com>
2026-06-09 16:09:52 +0530

    Add files via upload

---
+++ data_scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = soup.find_all("span", class_="text")
+
+print("Quotes from Website:\n")
+
+for i, quote in enumerate(quotes, start=1):
+    print(f"{i}. {quote.text}")
